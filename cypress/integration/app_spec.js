65d8155a4c968134c2715621aa74e027bc983b79
Charles De Mount <thexrayone@icloud.com>
2021-05-26 22:30:19 -0400

    test(integration): :white_check_mark: fix flaky test
    wait longer for list element to appear in the dom

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,28 +1,27 @@
 describe('Sign Up', () => {
   it('Adds person to course', () => {
-    cy.visit('/')
+    cy.visit('/');
 
     cy.get('input[name="name"]')
       .click()
       .type('Some Name')
-      .should('have.value', 'Some Name')
+      .should('have.value', 'Some Name');
 
-    cy.get('input[name="email"]')
-      .click()
-      .type('some@email.com')
+    cy.get('input[name="email"]').click().type('some@email.com');
 
     cy.get('select[name="department"]')
       .select('core')
-      .should('have.value', 'core')
+      .should('have.value', 'core');
 
     cy.get('select[name="course"]')
       .select('git-it')
-      .should('have.value', 'git-it')
+      .should('have.value', 'git-it');
 
-    cy.get('input[type="submit"]')
-      .click()
+    cy.get('input[type="submit"]').click();
 
-    cy.get('li')
-      .should('contain', 'Some Name - some@email.com - core - git-it')
-  })
-})
+    cy.get('li', { timeout: 4500 }).should(
+      'contain',
+      'Some Name - some@email.com - core - git-it',
+    );
+  });
+});
